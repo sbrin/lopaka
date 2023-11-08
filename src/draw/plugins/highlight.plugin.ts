9984c5dfed5944de12eccf3d025c7a74a514dcb7
deadlink <deadlink182@gmail.com>
2023-11-08 14:44:19 +0400

    improve highlights

--- src/draw/plugins/highlight.plugin.ts
+++ src/draw/plugins/highlight.plugin.ts
@@ -3,38 +3,38 @@ import {DrawPlugin} from './draw.plugin';
 
 export class HighlightPlugin extends DrawPlugin {
     public update(ctx: CanvasRenderingContext2D, point: Point): void {
-        if (!point || this.session.editor.state.activeTool) return;
+        if (this.session.editor.state.activeTool) return;
         const {scale, layers} = this.session.state;
         ctx.save();
         ctx.translate(0.5, 0.5);
         ctx.beginPath();
-        const hovered = layers
-            .filter((l) => l.contains(point.clone().divide(scale).round()))
-            .sort((a, b) => b.index - a.index);
         layers.forEach((layer) => {
             const bounds = layer.bounds.clone().multiply(scale).round().add(-1, -1, 1, 1);
-            if (layer.isEditing()) {
+            if (layer.selected) {
                 ctx.save();
                 ctx.strokeStyle = 'rgba(255, 255, 255, 1)';
                 ctx.lineWidth = 1;
                 ctx.setLineDash([5, 5]);
-                ctx.strokeRect(bounds.x, bounds.y, bounds.w, bounds.h);
-                ctx.restore();
-            } else if (layer.selected) {
-                ctx.save();
-                ctx.strokeStyle = 'rgba(255, 255, 255, 1)';
-                ctx.lineWidth = 1;
-                ctx.setLineDash([5, 5]);
-                ctx.strokeRect(bounds.x, bounds.y, bounds.w, bounds.h);
-                ctx.restore();
-            } else if (hovered.length && hovered[0] === layer) {
-                ctx.save();
-                ctx.strokeStyle = 'rgba(255, 255, 255, 0.6)';
-                ctx.lineWidth = 1;
                 ctx.strokeRect(bounds.x, bounds.y, bounds.w, bounds.h);
                 ctx.restore();
             }
         });
+        if (point) {
+            const hovered = layers
+                .filter((l) => l.contains(point.clone().divide(scale).round()))
+                .sort((a, b) => b.index - a.index);
+            if (hovered.length) {
+                const upperLayer = hovered[0];
+                if (!upperLayer.selected) {
+                    const bounds = upperLayer.bounds.clone().multiply(scale).round().add(-1, -1, 1, 1);
+                    ctx.save();
+                    ctx.strokeStyle = 'rgba(255, 255, 255, 0.6)';
+                    ctx.lineWidth = 1;
+                    ctx.strokeRect(bounds.x, bounds.y, bounds.w, bounds.h);
+                    ctx.restore();
+                }
+            }
+        }
         ctx.strokeStyle = 'rgba(255, 255, 255, 0.6)';
         ctx.lineWidth = 1;
         ctx.stroke();
