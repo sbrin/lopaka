5de3483285becd16b82c60f900546f0c3e69b526
deadlink <deadlink182@gmail.com>
2023-11-12 18:03:10 +0400

    resize improvements

--- src/draw/plugins/resize-icons.plugin.ts
+++ src/draw/plugins/resize-icons.plugin.ts
@@ -2,7 +2,6 @@ import {Point} from '../../core/point';
 import {DrawPlugin} from './draw.plugin';
 
 export class ResizeIconsPlugin extends DrawPlugin {
-    private iconSize: number = 3;
     update(ctx: CanvasRenderingContext2D, point: Point) {
         const {scale, layers} = this.session.state;
         const resizableLayers = layers.filter((l) => l.resizable && l.selected);
@@ -14,10 +13,11 @@ export class ResizeIconsPlugin extends DrawPlugin {
                 const r = editPoint.getRect().multiply(scale).round();
                 const c = r.getCenter();
                 ctx.moveTo(c.x, c.y);
-                ctx.arc(c.x, c.y, 3, 0, 2 * Math.PI);
+                ctx.arc(c.x, c.y, 4, 0, 2 * Math.PI);
             });
-            ctx.fillStyle = 'rgba(255, 255, 255, 0.9)';
-            ctx.fill();
+            ctx.strokeStyle = 'rgba(255, 255, 255, 0.7)';
+            ctx.lineWidth = 1;
+            ctx.stroke();
             ctx.restore();
         }
     }
