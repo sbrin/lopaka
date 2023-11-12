a6af33ea9a26a02c913771e4f13027433a952850
deadlink <deadlink182@gmail.com>
2023-11-12 23:14:05 +0400

    Update tests

--- src/platforms/layers.mock.ts
+++ src/platforms/layers.mock.ts
@@ -1,137 +1,110 @@
-export const layersMock: TLayer[] = [
+import {AbstractLayer, TLayerState} from '../core/layers/abstract.layer';
+import {LayerClassMap} from '../core/session';
+
+export const layersMock: AbstractLayer[] = [
     {
-        name: 'box_veqtjp8jf9ln6isyfz',
-        type: 'box',
-        index: 0,
-        x: 107,
-        y: 46,
-        id: 'veqtjp8jf9ln6isyfz',
-        width: 18,
-        height: 15,
+        n: 'box_veqtjp8jf9ln6isyfz',
+        t: 'box',
+        i: 0,
+        p: [107, 46],
+        u: 'veqtjp8jf9ln6isyfz',
+        s: [18, 15]
+    },
+    {
+        n: 'box_utlxpqvhdxnln6it5pw',
+        t: 'box',
+        i: 1,
+        p: [97, 2],
+        u: 'utlxpqvhdxnln6it5pw',
+        s: [27, 11]
+    },
+    {
+        n: 'box_eu6bpe0xpvvln6itc2e',
+        t: 'box',
+        i: 2,
+        p: [106, 19],
+        u: 'eu6bpe0xpvvln6itc2e',
+        s: [19, 18],
         radius: 0
     },
     {
-        name: 'box_utlxpqvhdxnln6it5pw',
-        type: 'box',
-        index: 1,
-        x: 97,
-        y: 2,
-        id: 'utlxpqvhdxnln6it5pw',
-        width: 27,
-        height: 11,
-        radius: 0
+        n: 'str_s3m4u762qpln6jvm90',
+        t: 'string',
+        i: 5,
+        p: [3, 11],
+        u: 's3m4u762qpln6jvm90',
+        d: 'Text',
+        s: [50, 8],
+        f: 'helvB08_tr'
     },
     {
-        name: 'box_eu6bpe0xpvvln6itc2e',
-        type: 'box',
-        index: 2,
-        x: 106,
-        y: 19,
-        id: 'eu6bpe0xpvvln6itc2e',
-        width: 19,
-        height: 18,
-        radius: 0
+        n: 'line_b11j6xs5t46ln6jw96h',
+        t: 'line',
+        i: 8,
+        p1: [2, 16],
+        u: 'b11j6xs5t46ln6jw96h',
+        p2: [68, 16]
     },
     {
-        name: 'str_s3m4u762qpln6jvm90',
-        type: 'text',
-        index: 5,
-        x: 3,
-        y: 11,
-        id: 's3m4u762qpln6jvm90',
-        yy: 3,
-        text: 'Text',
-        width: 50,
-        height: 8,
-        font: 'helvB08_tr'
+        n: 'dot_6n8rac0nxpcln6jwdwr',
+        t: 'dot',
+        i: 9,
+        p: [62, 4],
+        u: '6n8rac0nxpcln6jwdwr',
+        s: [1, 1]
     },
     {
-        name: 'line_b11j6xs5t46ln6jw96h',
-        type: 'line',
-        index: 8,
-        x: 2,
-        y: 16,
-        id: 'b11j6xs5t46ln6jw96h',
-        x2: 68,
-        y2: 16,
-        width: 0,
-        height: 0
+        n: 'dot_ema2hee85mln6jweht',
+        t: 'dot',
+        i: 10,
+        p: [67, 8],
+        u: 'ema2hee85mln6jweht',
+        s: [1, 1]
     },
     {
-        name: 'dot_6n8rac0nxpcln6jwdwr',
-        type: 'dot',
-        index: 9,
-        x: 62,
-        y: 4,
-        id: '6n8rac0nxpcln6jwdwr',
-        width: 1,
-        height: 1,
-        radius: 0
+        n: 'dot_j1ol1lo5nkln6jwfmg',
+        t: 'dot',
+        i: 11,
+        p: [72, 12],
+        u: 'j1ol1lo5nkln6jwfmg',
+        s: [1, 1]
     },
     {
-        name: 'dot_ema2hee85mln6jweht',
-        type: 'dot',
-        index: 10,
-        x: 67,
-        y: 8,
-        id: 'ema2hee85mln6jweht',
-        width: 1,
-        height: 1,
-        radius: 0
+        n: 'circle_6uf3k6wj82ln6jwi73',
+        t: 'circle',
+        i: 12,
+        p: [8, 29],
+        u: '6uf3k6wj82ln6jwi73',
+        r: 12
     },
     {
-        name: 'dot_j1ol1lo5nkln6jwfmg',
-        type: 'dot',
-        index: 11,
-        x: 72,
-        y: 12,
-        id: 'j1ol1lo5nkln6jwfmg',
-        width: 1,
-        height: 1,
-        radius: 0
+        n: 'disc_aibkefmz3cln6jwlit',
+        t: 'disc',
+        i: 13,
+        p: [14, 40],
+        u: 'aibkefmz3cln6jwlit',
+        r: 3
     },
     {
-        name: 'circle_6uf3k6wj82ln6jwi73',
-        type: 'circle',
-        index: 12,
-        x: 9,
-        y: 30,
-        id: '6uf3k6wj82ln6jwi73',
-        width: 24,
-        height: 24,
-        radius: 12
+        n: 'frame_g7pk1wfbqqkln6jwxby',
+        t: 'frame',
+        i: 14,
+        p: [44, 29],
+        u: 'g7pk1wfbqqkln6jwxby',
+        s: [44, 28]
     },
     {
-        name: 'disc_aibkefmz3cln6jwlit',
-        type: 'disc',
-        index: 13,
-        x: 15,
-        y: 41,
-        id: 'aibkefmz3cln6jwlit',
-        width: 6,
-        height: 6,
-        radius: 3
-    },
-    {
-        name: 'frame_g7pk1wfbqqkln6jwxby',
-        type: 'frame',
-        index: 14,
-        x: 44,
-        y: 29,
-        id: 'g7pk1wfbqqkln6jwxby',
-        width: 44,
-        height: 28,
-        radius: 0
-    },
-    {
-        name: 'draw_rrsjcz4oz2ln6jx4y4',
-        type: 'bitmap',
-        index: 15,
-        x: 0,
-        y: 0,
-        id: 'rrsjcz4oz2ln6jx4y4',
-        width: 128,
-        height: 64,
-        data: new ImageData(128, 64)
+        n: 'draw_rrsjcz4oz2ln6jx4y4',
+        t: 'paint',
+        i: 15,
+        p: [0, 0],
+        u: 'rrsjcz4oz2ln6jx4y4',
+        s: [128, 64],
+        d: []
     }
-];
+].map((l) => {
+    const type: ELayerType = l.t as any;
+    const layer = new LayerClassMap[type]();
+    layer.loadState(l);
+    return layer;
+});
