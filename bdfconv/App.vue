32fda0fcce87cbeef636035503467751264ce520
Mikahil Ilin <milin@evolution.com>
2023-12-08 10:54:31 +0000

    file output draft, has some issues with onExit

--- bdfconv/App.vue
+++ bdfconv/App.vue
@@ -1,11 +1,13 @@
 <script lang="ts" setup>
 import { computed, ref } from "vue";
-import { loadAndRun } from './app';
+import Module from './wasm/bdfconv';
 
 const textInput = ref('');
 const fileUploaded = ref(null);
 const args = computed(() => textInput.value.split(" "));
 const fileInput = ref(null);
+const outputFile = ref();
+const outputUrl = ref();
 
 function onClick() {
     loadAndRun(args.value, fileUploaded.value);
@@ -17,10 +19,67 @@ async function onFileChange(e) {
         return;
     }
     fileUploaded.value = file;
+    console.log("fileUploaded", file);
 }
 
 function resetFileInput() {
     fileInput.value = null;
+    outputFile.value = null;
+    outputUrl.value = null;
+}
+
+let moduleInstance = null;
+
+Module({
+    noInitialRun: true,
+    thisProgram: false,
+}).then((instance) => {
+    moduleInstance = instance;
+    console.log("Module created", moduleInstance);
+    moduleInstance.onExit = onExit;
+    moduleInstance.onAbort = onExit;
+});
+
+function onExit() {
+    console.log("onExit");
+    try {
+        // Assuming the output file has the same name but with a .u8g extension
+        outputFile.value = args[0] + '.u8g';
+        const outputFileData = moduleInstance?.FS.readFile(outputFile);
+        const blob = new Blob([outputFileData], { type: 'application/octet-stream' });
+        outputUrl.value = URL.createObjectURL(blob);
+    } catch (err) {
+        console.error('Error reading output file:', err);
+    }
+}
+
+async function loadAndRun(args, file) {
+    if (!file) {
+        return;
+    }
+    console.log("loadAndRun", file.name);
+    await new Promise((resolve) => {
+        const reader = new FileReader();
+        console.log("Promise");
+        reader.onload = (e) => {
+            console.log("onload", e.target?.result);
+            const data = new Uint8Array(e.target?.result as ArrayBufferLike);
+            moduleInstance?.FS.writeFile(file.name, data);
+            resolve(null);
+        };
+        reader.readAsArrayBuffer(file);
+    });
+
+    runMain([...args, file.name]);
+}
+    
+function runMain(args) {
+  if (moduleInstance) {
+    moduleInstance.callMain(args);
+    console.log("callMain", args);
+  } else {
+    console.error("Module is not initialized.");
+  }
 }
 
 </script>
@@ -40,6 +99,9 @@ function resetFileInput() {
         <div>{{ args }}</div>
         <div>
             <button @click="onClick">Run</button>
+        </div>
+        <div v-if="outputFile">
+            <a :href="outputUrl">{{ outputFile }}</a>
         </div>
     </div>
 </template>
--- bdfconv/app.ts
+++ bdfconv/app.ts
@@ -1,37 +1,4 @@
 import { createApp } from "vue";
-import Module from './wasm/bdfconv';
 import AppVue from "./App.vue";
-
-let moduleInstance = null;
-
-Module({
-    noInitialRun: true,
-    thisProgram: false,
-}).then((instance) => {
-    moduleInstance = instance;
-});
-
-export async function loadAndRun(args, file) {
-    const filePath = file.name;
-    await new Promise((resolve) => {
-        const reader = new FileReader();
-        reader.onload = (e) => {
-            const data = new Uint8Array(e.target.result);
-            moduleInstance.FS.writeFile(filePath, data);
-            resolve(null);
-        };
-        reader.readAsArrayBuffer(file);
-    });
-
-    runMain([...args, filePath]);
-}
-    
-export function runMain(args) {
-  if (moduleInstance) {
-    moduleInstance.callMain(args);
-  } else {
-    console.error("Module is not initialized.");
-  }
-}
 
 createApp(AppVue).mount('#bdfconv_app');
