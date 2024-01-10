1669be6f45f4b7b9f543b2df62239cd148e0b28d
Mikahil Ilin <milin@evolution.com>
2024-01-10 15:17:22 +0000

    custom display size dialog draft

---
+++ src/components/fui/FuiPopup.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup>
+import {defineProps} from 'vue';
+
+const props = defineProps<{
+    active?: boolean;
+}>();
+</script>
+<template>
+    <div class="shadow">
+        <div class="popup">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+<style lang="css">
+.shadow {
+    z-index: 100;
+    position: fixed;
+    left: 0;
+    top:0;
+    background-color: rgba(0,0,0,0.7);
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: flex-start;
+    justify-content: center;
+}
+.popup {
+    margin-top: 20%;
+    background: var(--bg-color);
+    padding: 20px;
+    border-radius: 8px;
+
+}
+</style>
