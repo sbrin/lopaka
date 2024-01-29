e00a20130432f4e3060f01d316645b2a9bb5828f
Gaai <59128156+Gaai@users.noreply.github.com>
2024-01-29 20:35:23 +0100

    Added more + some minor modifications

--- src/icons/gaai.ts
+++ src/icons/gaai.ts
@@ -1,5 +1,20 @@
-import Alarm_Clock_13x16 from '../../img/gaai/alarm_clock_13x16.png';
-import Alarm_Clock_Twinkle_13x16 from '../../img/gaai/alarm_clock_twinkle_13x16.png';
+import Alarm_Clock_15x16 from '../../img/gaai/alarm_clock_15x16.png';
+import Arrow_Curved_Down_Left_5x8 from '../../img/gaai/arrow_curved_down_left_5x8.png';
+import Arrow_Curved_Down_Right_5x8 from '../../img/gaai/arrow_curved_down_right_5x8.png';
+import Arrow_Curved_Left_Down_5x7 from '../../img/gaai/arrow_curved_left_down_5x7.png';
+import Arrow_Curved_Left_Down_8x5 from '../../img/gaai/arrow_curved_left_down_8x5.png';
+import Arrow_Curved_Left_Down_Up_5x7 from '../../img/gaai/arrow_curved_left_down_up_5x7.png';
+import Arrow_Curved_Left_Up_5x7 from '../../img/gaai/arrow_curved_left_up_5x7.png';
+import Arrow_Curved_Left_Up_8x5 from '../../img/gaai/arrow_curved_left_up_8x5.png';
+import Arrow_Curved_Left_Up_Down_7x5 from '../../img/gaai/arrow_curved_left_up_down_7x5.png';
+import Arrow_Curved_Right_Down_5x7 from '../../img/gaai/arrow_curved_right_down_5x7.png';
+import Arrow_Curved_Right_Down_8x5 from '../../img/gaai/arrow_curved_right_down_8x5.png';
+import Arrow_Curved_Right_Down_Up_7x5 from '../../img/gaai/arrow_curved_right_down_up_7x5.png';
+import Arrow_Curved_Right_Up_5x7 from '../../img/gaai/arrow_curved_right_up_5x7.png';
+import Arrow_Curved_Right_Up_8x5 from '../../img/gaai/arrow_curved_right_up_8x5.png';
+import Arrow_Curved_Right_Up_Down_7x5 from '../../img/gaai/arrow_curved_right_up_down_7x5.png';
+import Arrow_Curved_Up_Left_5x8 from '../../img/gaai/arrow_curved_up_left_5x8.png';
+import Arrow_Curved_Up_Right_5x8 from '../../img/gaai/arrow_curved_up_right_5x8.png';
 import Battery_0_24x16 from '../../img/gaai/battery_0_24x16.png';
 import Battery_100_24x16 from '../../img/gaai/battery_100_24x16.png';
 import Battery_10_24x16 from '../../img/gaai/battery_10_24x16.png';
@@ -28,23 +43,41 @@ import Brightness_15x16 from '../../img/gaai/brightness_15x16.png';
 import Broadcast_15x16 from '../../img/gaai/broadcast_15x16.png';
 import Bullet_Off_15x16 from '../../img/gaai/bullet_off_15x16.png';
 import Bullet_On_15x16 from '../../img/gaai/bullet_on_15x16.png';
+import Calendar_15x16 from '../../img/gaai/calendar_15x16.png';
 import Call_Incoming_15x16 from '../../img/gaai/call_incoming_15x16.png';
 import Call_In_Out_15x16 from '../../img/gaai/call_in_out_15x16.png';
 import Call_In_Progress2_15x16 from '../../img/gaai/call_in_progress2_15x16.png';
 import Call_In_Progress_15x16 from '../../img/gaai/call_in_progress_15x16.png';
 import Call_Outgoing_15x16 from '../../img/gaai/call_outgoing_15x16.png';
+import Camera_17x16 from '../../img/gaai/camera_17x16.png';
 import Checked_14x16 from '../../img/gaai/checked_14x16.png';
 import Check_12x16 from '../../img/gaai/check_12x16.png';
 import Clockface_15x16 from '../../img/gaai/clockface_15x16.png';
 import Clockface_Quarters_15x16 from '../../img/gaai/clockface_quarters_15x16.png';
 import Clock_15x16 from '../../img/gaai/clock_15x16.png';
 import Clock_Quarters_15x16 from '../../img/gaai/clock_quarters_15x16.png';
+import Cloud_17x16 from '../../img/gaai/cloud_17x16.png';
+import Cloud_Download_17x16 from '../../img/gaai/cloud_download_17x16.png';
+import Cloud_Download_Fat_17x16 from '../../img/gaai/cloud_download_fat_17x16.png';
+import Cloud_Lightning_Bolt_17x16 from '../../img/gaai/cloud_lightning_bolt_17x16.png';
+import Cloud_Rain3_17x16 from '../../img/gaai/cloud_rain3_17x16.png';
+import Cloud_Snow_17x16 from '../../img/gaai/cloud_snow_17x16.png';
+import Cloud_Sunny_17x16 from '../../img/gaai/cloud_sunny_17x16.png';
+import Cloud_Sync_17x16 from '../../img/gaai/cloud_sync_17x16.png';
+import Cloud_Upload_2_17x16 from '../../img/gaai/cloud_upload_2_17x16.png';
+import Contacts_14x16 from '../../img/gaai/contacts_14x16.png';
+import Contacts_15x16 from '../../img/gaai/contacts_15x16.png';
 import Contrast_15x16 from '../../img/gaai/contrast_15x16.png';
 import Crossed_11x16 from '../../img/gaai/crossed_11x16.png';
 import Crossed_13x16 from '../../img/gaai/crossed_13x16.png';
 import Crossed_15x16 from '../../img/gaai/crossed_15x16.png';
 import Cross_11x16 from '../../img/gaai/cross_11x16.png';
 import Cross_9x16 from '../../img/gaai/cross_9x16.png';
+import Date_Day_15x16 from '../../img/gaai/date_day_15x16.png';
+import Date_Day_Empty_15x16 from '../../img/gaai/date_day_empty_15x16.png';
+import Delete_Bin_13x16 from '../../img/gaai/delete_bin_13x16.png';
+import Door_12x16 from '../../img/gaai/door_12x16.png';
+import Download_15x16 from '../../img/gaai/download_15x16.png';
 import Envelope_17x16 from '../../img/gaai/envelope_17x16.png';
 import Error_15x16 from '../../img/gaai/error_15x16.png';
 import Error_Sign_15x16 from '../../img/gaai/error_sign_15x16.png';
@@ -52,11 +85,32 @@ import Fast_Backward_17x16 from '../../img/gaai/fast_backward_17x16.png';
 import Fast_Backward_Button_15x16 from '../../img/gaai/fast_backward_button_15x16.png';
 import Fast_Forward_17x16 from '../../img/gaai/fast_forward_17x16.png';
 import Fast_Forward_Button_15x16 from '../../img/gaai/fast_forward_button_15x16.png';
+import File_12x16 from '../../img/gaai/file_12x16.png';
+import Flag_16x16 from '../../img/gaai/flag_16x16.png';
+import Flag_Stick_17x16 from '../../img/gaai/flag_stick_17x16.png';
 import Folder_Explorer_17x16 from '../../img/gaai/folder_explorer_17x16.png';
 import Forbidden_Sign_15x16 from '../../img/gaai/forbidden_sign_15x16.png';
+import Frost_15x16 from '../../img/gaai/frost_15x16.png';
+import Hart_15x16 from '../../img/gaai/hart_15x16.png';
 import Headphones_15x16 from '../../img/gaai/headphones_15x16.png';
+import Help_Sign_12x15 from '../../img/gaai/help_sign_12x15.png';
+import Help_Sign_Black_15x16 from '../../img/gaai/help_sign_black_15x16.png';
+import Help_Sign_White_15x16 from '../../img/gaai/help_sign_white_15x16.png';
+import Home_15x16 from '../../img/gaai/home_15x16.png';
+import Humidity_11x16 from '../../img/gaai/humidity_11x16.png';
+import Humidity_White_11x16 from '../../img/gaai/humidity_white_11x16.png';
+import Information_Sign from '../../img/gaai/information_sign.png';
+import Information_Sign_15x16 from '../../img/gaai/information_sign_15x16.png';
+import Information_Sign_White_15x16 from '../../img/gaai/information_sign_white_15x16.png';
+import Key_15x16 from '../../img/gaai/key_15x16.png';
 import Light_16x16 from '../../img/gaai/light_16x16.png';
+import Link_15x16 from '../../img/gaai/link_15x16.png';
+import Location_13x16 from '../../img/gaai/location_13x16.png';
+import Location_Map_15x16 from '../../img/gaai/location_map_15x16.png';
+import Lock_13x16 from '../../img/gaai/lock_13x16.png';
 import Mail_17x16 from '../../img/gaai/mail_17x16.png';
+import Map_18x16 from '../../img/gaai/map_18x16.png';
+import Map_Arrow_18x16 from '../../img/gaai/map_arrow_18x16.png';
 import Memo_17x16 from '../../img/gaai/memo_17x16.png';
 import Memo_Recorder_12x16 from '../../img/gaai/memo_recorder_12x16.png';
 import Menu_16x16 from '../../img/gaai/menu_16x16.png';
@@ -71,9 +125,11 @@ import Menu_Arrow_Up_Right_4x4 from '../../img/gaai/menu_arrow_up_right_4x4.png'
 import Message_16x16 from '../../img/gaai/message_16x16.png';
 import Microphone_15x16 from '../../img/gaai/microphone_15x16.png';
 import Microphone_Muted_15x16 from '../../img/gaai/microphone_muted_15x16.png';
-import Microphone_Recording_17x16 from '../../img/gaai/microphone_recording_17x16.png';
+import Microphone_Recording_15x16 from '../../img/gaai/microphone_recording_15x16.png';
 import Micro_Sd_14x16 from '../../img/gaai/micro_sd_14x16.png';
 import Micro_Sd_No_Card_14x16 from '../../img/gaai/micro_sd_no_card_14x16.png';
+import Moon_16x16 from '../../img/gaai/moon_16x16.png';
+import Moon_White_16x16 from '../../img/gaai/moon_white_16x16.png';
 import Music_14x16 from '../../img/gaai/music_14x16.png';
 import Network_15x16 from '../../img/gaai/network_15x16.png';
 import Network_1_Bar_15x16 from '../../img/gaai/network_1_bar_15x16.png';
@@ -81,11 +137,16 @@ import Network_2_Bars_15x16 from '../../img/gaai/network_2_bars_15x16.png';
 import Network_3_Bars_15x16 from '../../img/gaai/network_3_bars_15x16.png';
 import Network_4_Bars_15x16 from '../../img/gaai/network_4_bars_15x16.png';
 import Network_Not_Connected_15x16 from '../../img/gaai/network_not_connected_15x16.png';
+import Next_16x16 from '../../img/gaai/next_16x16.png';
+import Next_Button_15x16 from '../../img/gaai/next_button_15x16.png';
 import Notification_Bell_16x16 from '../../img/gaai/notification_bell_16x16.png';
 import Open_File_18x16 from '../../img/gaai/open_file_18x16.png';
 import Open_Folder_18x16 from '../../img/gaai/open_folder_18x16.png';
 import Pause_12x16 from '../../img/gaai/pause_12x16.png';
 import Pause_Button_15x16 from '../../img/gaai/pause_button_15x16.png';
+import Phone_Book_Closed_15x16 from '../../img/gaai/phone_book_closed_15x16.png';
+import Phone_Book_Open_17x16 from '../../img/gaai/phone_book_open_17x16.png';
+import Phone_Book_Open_Tall_17x16 from '../../img/gaai/phone_book_open_tall_17x16.png';
 import Phone_Connected_15x16 from '../../img/gaai/phone_connected_15x16.png';
 import Phone_Hook_20x16 from '../../img/gaai/phone_hook_20x16.png';
 import Phone_Not_Connected_15x16 from '../../img/gaai/phone_not_connected_15x16.png';
@@ -93,22 +154,37 @@ import Phone_On_Hook_21x16 from '../../img/gaai/phone_on_hook_21x16.png';
 import Play_15x16 from '../../img/gaai/play_15x16.png';
 import Play_Button_15x16 from '../../img/gaai/play_button_15x16.png';
 import Power_Button_15x16 from '../../img/gaai/power_button_15x16.png';
+import Previous_16x16 from '../../img/gaai/previous_16x16.png';
+import Previous_Button_15x16 from '../../img/gaai/previous_button_15x16.png';
 import Radio_16x16 from '../../img/gaai/radio_16x16.png';
 import Record_Button_15x16 from '../../img/gaai/record_button_15x16.png';
-import Right_15x16 from '../../img/gaai/right_15x16.png';
+import Refresh_13x16 from '../../img/gaai/refresh_13x16.png';
+import Reset_11x16 from '../../img/gaai/reset_11x16.png';
+import Reset_13x16 from '../../img/gaai/reset_13x16.png';
+import Right_16x16 from '../../img/gaai/right_16x16.png';
 import Save_16x16 from '../../img/gaai/save_16x16.png';
 import Save_Flipped_16x16 from '../../img/gaai/save_flipped_16x16.png';
+import Search_Big from '../../img/gaai/search_big.png';
 import Settings_Gear_16x16 from '../../img/gaai/settings_gear_16x16.png';
 import Settings_Sliders_14x16 from '../../img/gaai/settings_sliders_14x16.png';
 import Settings_Sliders_Square_14x16 from '../../img/gaai/settings_sliders_square_14x16.png';
 import Settings_Sliders_Two_14x16 from '../../img/gaai/settings_sliders_two_14x16.png';
+import Sleep_Mode_Black_15x16 from '../../img/gaai/sleep_mode_black_15x16.png';
+import Sleep_Mode_White_15x16 from '../../img/gaai/sleep_mode_white_15x16.png';
 import Sound_Wave_17x16 from '../../img/gaai/sound_wave_17x16.png';
 import Speaker_19x16 from '../../img/gaai/speaker_19x16.png';
+import Star_15x16 from '../../img/gaai/star_15x16.png';
 import Stop_14x16 from '../../img/gaai/stop_14x16.png';
 import Stop_Button_15x16 from '../../img/gaai/stop_button_15x16.png';
 import Streaming_17x16 from '../../img/gaai/streaming_17x16.png';
+import Sun_15x16 from '../../img/gaai/sun_15x16.png';
+import Temperature_16x16 from '../../img/gaai/temperature_16x16.png';
 import Tools_14x16 from '../../img/gaai/tools_14x16.png';
 import Tool_Wrench_14x16 from '../../img/gaai/tool_wrench_14x16.png';
+import Tv_17x16 from '../../img/gaai/tv_17x16.png';
+import Undo_12x16 from '../../img/gaai/undo_12x16.png';
+import Upload_15x16 from '../../img/gaai/upload_15x16.png';
+import Video_Camera_19x16 from '../../img/gaai/video_camera_19x16.png';
 import Volume_1_Bar_20x16 from '../../img/gaai/volume_1_bar_20x16.png';
 import Volume_2_Bars_20x16 from '../../img/gaai/volume_2_bars_20x16.png';
 import Volume_3_Bars_20x16 from '../../img/gaai/volume_3_bars_20x16.png';
@@ -131,8 +207,23 @@ import Wifi_Not_Connected_19x16 from '../../img/gaai/wifi_not_connected_19x16.pn
 import Wrong_15x16 from '../../img/gaai/wrong_15x16.png';
 
 export default { 
-    Alarm_Clock_13x16, 
-    Alarm_Clock_Twinkle_13x16, 
+    Alarm_Clock_15x16, 
+    Arrow_Curved_Down_Left_5x8, 
+    Arrow_Curved_Down_Right_5x8, 
+    Arrow_Curved_Left_Down_5x7, 
+    Arrow_Curved_Left_Down_8x5, 
+    Arrow_Curved_Left_Down_Up_5x7, 
+    Arrow_Curved_Left_Up_5x7, 
+    Arrow_Curved_Left_Up_8x5, 
+    Arrow_Curved_Left_Up_Down_7x5, 
+    Arrow_Curved_Right_Down_5x7, 
+    Arrow_Curved_Right_Down_8x5, 
+    Arrow_Curved_Right_Down_Up_7x5, 
+    Arrow_Curved_Right_Up_5x7, 
+    Arrow_Curved_Right_Up_8x5, 
+    Arrow_Curved_Right_Up_Down_7x5, 
+    Arrow_Curved_Up_Left_5x8, 
+    Arrow_Curved_Up_Right_5x8, 
     Battery_0_24x16, 
     Battery_100_24x16, 
     Battery_10_24x16, 
@@ -161,23 +252,41 @@ export default {
     Broadcast_15x16, 
     Bullet_Off_15x16, 
     Bullet_On_15x16, 
+    Calendar_15x16, 
     Call_Incoming_15x16, 
     Call_In_Out_15x16, 
     Call_In_Progress2_15x16, 
     Call_In_Progress_15x16, 
     Call_Outgoing_15x16, 
+    Camera_17x16, 
     Checked_14x16, 
     Check_12x16, 
     Clockface_15x16, 
     Clockface_Quarters_15x16, 
     Clock_15x16, 
     Clock_Quarters_15x16, 
+    Cloud_17x16, 
+    Cloud_Download_17x16, 
+    Cloud_Download_Fat_17x16, 
+    Cloud_Lightning_Bolt_17x16, 
+    Cloud_Rain3_17x16, 
+    Cloud_Snow_17x16, 
+    Cloud_Sunny_17x16, 
+    Cloud_Sync_17x16, 
+    Cloud_Upload_2_17x16, 
+    Contacts_14x16, 
+    Contacts_15x16, 
     Contrast_15x16, 
     Crossed_11x16, 
     Crossed_13x16, 
     Crossed_15x16, 
     Cross_11x16, 
     Cross_9x16, 
+    Date_Day_15x16, 
+    Date_Day_Empty_15x16, 
+    Delete_Bin_13x16, 
+    Door_12x16, 
+    Download_15x16, 
     Envelope_17x16, 
     Error_15x16, 
     Error_Sign_15x16, 
@@ -185,11 +294,32 @@ export default {
     Fast_Backward_Button_15x16, 
     Fast_Forward_17x16, 
     Fast_Forward_Button_15x16, 
+    File_12x16, 
+    Flag_16x16, 
+    Flag_Stick_17x16, 
     Folder_Explorer_17x16, 
     Forbidden_Sign_15x16, 
+    Frost_15x16, 
+    Hart_15x16, 
     Headphones_15x16, 
+    Help_Sign_12x15, 
+    Help_Sign_Black_15x16, 
+    Help_Sign_White_15x16, 
+    Home_15x16, 
+    Humidity_11x16, 
+    Humidity_White_11x16, 
+    Information_Sign, 
+    Information_Sign_15x16, 
+    Information_Sign_White_15x16, 
+    Key_15x16, 
     Light_16x16, 
+    Link_15x16, 
+    Location_13x16, 
+    Location_Map_15x16, 
+    Lock_13x16, 
     Mail_17x16, 
+    Map_18x16, 
+    Map_Arrow_18x16, 
     Memo_17x16, 
     Memo_Recorder_12x16, 
     Menu_16x16, 
@@ -204,9 +334,11 @@ export default {
     Message_16x16, 
     Microphone_15x16, 
     Microphone_Muted_15x16, 
-    Microphone_Recording_17x16, 
+    Microphone_Recording_15x16, 
     Micro_Sd_14x16, 
     Micro_Sd_No_Card_14x16, 
+    Moon_16x16, 
+    Moon_White_16x16, 
     Music_14x16, 
     Network_15x16, 
     Network_1_Bar_15x16, 
@@ -214,11 +346,16 @@ export default {
     Network_3_Bars_15x16, 
     Network_4_Bars_15x16, 
     Network_Not_Connected_15x16, 
+    Next_16x16, 
+    Next_Button_15x16, 
     Notification_Bell_16x16, 
     Open_File_18x16, 
     Open_Folder_18x16, 
     Pause_12x16, 
     Pause_Button_15x16, 
+    Phone_Book_Closed_15x16, 
+    Phone_Book_Open_17x16, 
+    Phone_Book_Open_Tall_17x16, 
     Phone_Connected_15x16, 
     Phone_Hook_20x16, 
     Phone_Not_Connected_15x16, 
@@ -226,22 +363,37 @@ export default {
     Play_15x16, 
     Play_Button_15x16, 
     Power_Button_15x16, 
+    Previous_16x16, 
+    Previous_Button_15x16, 
     Radio_16x16, 
     Record_Button_15x16, 
-    Right_15x16, 
+    Refresh_13x16, 
+    Reset_11x16, 
+    Reset_13x16, 
+    Right_16x16, 
     Save_16x16, 
     Save_Flipped_16x16, 
+    Search_Big, 
     Settings_Gear_16x16, 
     Settings_Sliders_14x16, 
     Settings_Sliders_Square_14x16, 
     Settings_Sliders_Two_14x16, 
+    Sleep_Mode_Black_15x16, 
+    Sleep_Mode_White_15x16, 
     Sound_Wave_17x16, 
     Speaker_19x16, 
+    Star_15x16, 
     Stop_14x16, 
     Stop_Button_15x16, 
     Streaming_17x16, 
+    Sun_15x16, 
+    Temperature_16x16, 
     Tools_14x16, 
     Tool_Wrench_14x16, 
+    Tv_17x16, 
+    Undo_12x16, 
+    Upload_15x16, 
+    Video_Camera_19x16, 
     Volume_1_Bar_20x16, 
     Volume_2_Bars_20x16, 
     Volume_3_Bars_20x16, 
