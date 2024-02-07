692339886c4bd76dba1630a055550213ad9284f7
Mikahil Ilin <milin@evolution.com>
2024-02-07 09:50:41 +0000

    fix rebase mistake, change tools order

--- src/editor/editor.ts
+++ src/editor/editor.ts
@@ -47,12 +47,12 @@ export class Editor {
 
     tools: {[key: string]: AbstractTool} = {
         paint: new PaintTool(this),
-        dot: new DotTool(this),
-        line: new LineTool(this),
+        string: new TextTool(this),
         rect: new RectTool(this),
         circle: new CircleTool(this),
         ellipse: new EllipseTool(this),
-        string: new TextTool(this)
+        line: new LineTool(this),
+        dot: new DotTool(this),
     };
 
     getSupportedTools(platform: string): {[key: string]: AbstractTool} {
@@ -143,9 +143,7 @@ export class Editor {
                     this.onMouseDoubleClick(point, event);
                     break;
             }
-            if (!alienEvent) {
-                virtualScreen.updateMousePosition(screenPoint);
-            }
+            virtualScreen.updateMousePosition(screenPoint, event);
         }
     };
 
