676e6c5342c18567d1999fb98176a1fd84b0539a
deadlink <deadlink182@gmail.com>
2024-02-11 17:46:25 +0400

    Optimizing compiled font size

--- vite-plugins/bdf-format.ts
+++ vite-plugins/bdf-format.ts
@@ -8,14 +8,24 @@ const bdfFormatkPlugin: Plugin = {
     transform(code, id) {
         if (bdfExt.test(id)) {
             const fontData: any = parseBDF(code);
-            fontData.glyphs = Array.from(fontData.glyphs.entries());
+            fontData.glyphs = Array.from(fontData.glyphs.entries() as [string, any]).map(([key, value]) => [
+                value.code,
+                value.char,
+                value.scalableSize,
+                value.deviceSize,
+                value.bounds,
+                value.bytes
+            ]);
+
             return {
                 code: `
 /*
-    @file: ${id}
+    @file: ${id.split('/').pop()}
+    @name: ${fontData.meta.name}
+    @glyphs: ${fontData.meta.totalChars}
 */
 const fontData = ${JSON.stringify(fontData)};
-fontData.glyphs = new Map(fontData.glyphs);
+fontData.glyphs = new Map(fontData.glyphs.map(([code, char, scalableSize, deviceSize, bounds, bytes]) => ([code, {code,char,scalableSize,deviceSize,bounds,bytes: new Uint8Array(bytes)}])));
 export default fontData;`,
                 map: null
             };
