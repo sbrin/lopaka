44f1523a146b27d47d2e6f24ecdf66e5a73b4090
deadlink <deadlink182@gmail.com>
2024-02-12 19:37:34 +0400

    Obtaining icon size from image

--- vite-plugins/icons-pack.ts
+++ vite-plugins/icons-pack.ts
@@ -1,13 +1,13 @@
 import {Plugin} from 'vite';
 import path from 'path';
 import fs from 'fs';
-const ext = /\.pack$/;
+import probe from 'probe-image-size';
 const packExt = /\.icons\.pack$/;
 const iconReg = /(.*?)_(\d+)x(\d+).png/;
 
 const iconsPackPlugin: Plugin = {
     name: 'icons.pack',
-    transform(code, id) {
+    async transform(code, id) {
         if (packExt.test(id)) {
             const dir = path.dirname(id);
             const meta: any = code.split('\n').reduce((acc, cur) => {
@@ -16,22 +16,25 @@ const iconsPackPlugin: Plugin = {
                 }
                 return acc;
             }, {});
-
-            // read list of files from meta.source
             const sourceDir = path.resolve(dir, meta.source);
-            const icons = fs.readdirSync(sourceDir).reduce((acc: any[], cur) => {
-                const icon = fs.readFileSync(path.resolve(sourceDir, cur));
-                const iconData: any = iconReg.exec(cur);
+            const files = fs.readdirSync(sourceDir);
+            const icons = [];
+            for (let i = 0; i < files.length; i++) {
+                const fileName = path.resolve(sourceDir, files[i]);
+                const stream = fs.createReadStream(fileName);
+                const icon = fs.readFileSync(fileName);
+                const iconData: any = iconReg.exec(files[i]);
                 if (iconData) {
-                    acc.push({
+                    const info = await probe(stream);
+                    console.log(info);
+                    icons.push({
                         name: iconData[1],
-                        width: Number(iconData[2]),
-                        height: Number(iconData[3]),
-                        image: `data:image/png;base64,${icon.toString('base64')}`
+                        width: info.width,
+                        height: info.height,
+                        image: `data:${info.mime};base64,${icon.toString('base64')}`
                     });
                 }
-                return acc;
-            }, []);
+            }
             return {
                 code: `
 /*
