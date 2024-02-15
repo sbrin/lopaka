62976e3c1807da138e97c96eb72c63b07ad23731
Mikahil Ilin <milin@evolution.com>
2024-02-15 12:39:21 +0000

    refactor flipper fonts, add em to u8g2

--- src/platforms/u8g2.test.ts
+++ src/platforms/u8g2.test.ts
@@ -2,7 +2,7 @@ import {describe, expect, it} from 'vitest';
 import {layersMock} from './layers.mock';
 import {U8g2Platform} from './u8g2';
 
-describe('Flipper zero platform', () => {
+describe('U8g2 platform', () => {
     it('generating source code', () => {
         const platform = new U8g2Platform();
         const sourceCode = platform.generateSourceCode(layersMock);
