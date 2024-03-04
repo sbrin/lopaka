17abfca6c498b7dc60aca42a08be54c8f4549ccc
deadlink <deadlink182@gmail.com>
2024-03-05 01:53:08 +0400

    Inkplate template and colors

--- src/platforms/inkplate.ts
+++ src/platforms/inkplate.ts
@@ -1,9 +1,17 @@
 import {AdafruitPlatform} from './adafruit';
+import defaultTemplate from './templates/adafruit/inkplate.pug';
 
 export class InkplatePlatform extends AdafruitPlatform {
     public static id = 'inkplate';
     protected name = 'Inkplate';
     protected description = 'Inkplate';
+
+    protected templates = {
+        Default: {
+            template: defaultTemplate,
+            settings: {}
+        }
+    };
 
     constructor() {
         super();
@@ -23,7 +31,25 @@ export class InkplatePlatform extends AdafruitPlatform {
         });
     }
 
-    // protected packColor(color: string): string {
-    //     return '1';
-    // }
+    protected packColor(color: string): string {
+        switch (color) {
+            case '#000000':
+                return '0';
+            case '#111111':
+                return '6';
+            case '#333333':
+                return '5';
+            case '#555555':
+                return '4';
+            case '#777777':
+                return '3';
+            case '#999999':
+                return '2';
+            case '#bbbbbb':
+                return '1';
+            case '#ffffff':
+                return '7';
+        }
+        return '1';
+    }
 }
