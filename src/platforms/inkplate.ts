5feb710961b4be11ac957bdb86e4a7728f3bbfb9
deadlink <deadlink182@gmail.com>
2024-03-07 01:59:06 +0400

    Inkplate colors

--- src/platforms/inkplate.ts
+++ src/platforms/inkplate.ts
@@ -19,9 +19,9 @@ export class InkplatePlatform extends AdafruitPlatform {
         super();
         Object.assign(this.features, {
             hasIndexedColors: true,
-            palette: ['#000000', '#111111', '#333333', '#555555', '#777777', '#999999', '#bbbbbb', '#ffffff'],
+            palette: ['#000000', '#111111', '#333333', '#555555', '#777777', '#999999', '#bbbbbb', '#f8f8f8'],
             defaultColor: '#000000',
-            screenBgColor: '#e2eeee',
+            screenBgColor: '#f8f8f8',
             interfaceColors: {
                 selectColor: 'rgba(0, 0, 0, 0.9)',
                 resizeIconColor: 'rgba(0, 0, 0, 0.6)',
@@ -49,6 +49,7 @@ export class InkplatePlatform extends AdafruitPlatform {
                 return '5';
             case '#bbbbbb':
                 return '6';
+            case '#f8f8f8':
             case '#ffffff':
                 return '7';
         }
