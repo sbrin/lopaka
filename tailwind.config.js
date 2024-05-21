b98db802ac3ab6fc362cd644c98060497f5e7a3d
deadlink <deadlink182@gmail.com>
2024-05-22 00:13:24 +0400

    Fix tw config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,29 +4,11 @@ const typography = require('@tailwindcss/typography');
 
 module.exports = {
     content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-    darkMode: 'class', // or 'media' or 'class'
-    theme: {
-        extend: {}
-    },
-    variants: {
-        extend: {}
-    },
     plugins: [typography, daisyui],
-    corePlugins: {
-        preflight: false
-    },
     daisyui: {
-        darkTheme: 'dark', // name of one of the included themes for dark mode
-        base: true, // applies background color and foreground color for root element by default
-        styled: true, // include daisyUI colors and design decisions for all components
-        utils: true, // adds responsive and modifier utility classes
-        prefix: '', // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
-        logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
-        themeRoot: ':root',
         themes: [
             {
                 lopaka: {
-                    ...require('daisyui/src/theming/themes').dark,
                     primary: '#FF8200',
                     neutral: '#441F07',
                     'base-100': '#120800', // bg
